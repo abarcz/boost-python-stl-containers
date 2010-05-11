79918ad4dd38c8542eb602216511e864b52f7b79
michand <michand@77f40421-bdcf-6194-5f83-fd0082402d5a>
2010-05-11 15:10:01 +0000

    Kolejnych kilka linijek i prymitywne testy

---
+++ Source/test.py
@@ -0,0 +1,48 @@
+import zpr
+mapa = zpr.Map()
+wektor = zpr.Vec()
+set1 = zpr.Set()
+
+mapa[0] = "a"
+mapa[1] = "b"
+mapa[6] = "c"
+
+wektor.append(0)
+wektor.append(0)
+wektor.append(1)
+wektor.append(2)
+wektor.append(3)
+wektor.__len__()
+
+if wektor.__len__() != 5 :
+	print "Error"
+
+wektor.__delitem__(2)
+
+if wektor.__len__() != 4 :
+	print "Error"
+
+wektor[2] = 8
+
+if wektor[2] != 8 :
+	print "Error"
+
+wektor.clear()
+
+if wektor.__len__() != 0 :
+	print "Error"
+
+set1.append(0)
+set1.append(0)
+set1.append(0)
+set1.append(1)
+set1.__len__()
+
+if set1.__len__() != 2 :
+	print "Error"
+
+set1.clear()
+
+if set1.__len__() != 0 :
+	print "Error"
+
