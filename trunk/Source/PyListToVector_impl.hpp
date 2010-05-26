af5cd1d69ff2949767cc06efd8274270ec35d276
abarcz <abarcz@77f40421-bdcf-6194-5f83-fd0082402d5a>
2010-05-26 23:21:47 +0000

    Dodane: PyListToVector- konwerter listy na wektor
    TestModule - moduł pythonowy do wrappowania fcji potrzebnych do testów. Aktualnie wykorzystywany przez testy wykorzystania PyListToVector
    
    Python_test_py_lit_to_vector.py - testy konwertera dla int i double,
    przekazanie pythonowej listy do fcji typu fun(std::vector<T>) za pośrednictwem konwertera

---
+++ trunk/Source/PyListToVector_impl.hpp
@@ -0,0 +1,23 @@
+
+template<typename T>	
+std::vector<T> pyListToVector(boost::python::list pyList) {
+	std::vector<T> vec;
+    int length = boost::python::len(pyList);
+	for(int i = 0; i < length; i++) {
+        try {
+            boost::python::extract<T> elem(pyList[i]);
+            if(!elem.check()) {             //skip if element not convertible
+                std::cout << "pyListToVector: element at position "
+                << i << " couldn't be converted due to incorrect type." 
+                << std::endl;
+            } 
+            else
+                vec.push_back(elem);
+        } catch(...) {
+                std::cout << "pyListToVector: element at position "
+                << i << " couldn't be converted, "
+                << "probably due to surpassing type limits."<< std::endl;
+        }
+	}
+	return vec;
+}
