b131cee61e0ce51d5544e8a862276f2dc7d9254d
michand <michand@77f40421-bdcf-6194-5f83-fd0082402d5a>
2010-05-26 18:38:28 +0000

    Reorganizacja testow, Makefile

--- trunk/Source/Python_test_vectors.py
+++ trunk/Source/Python_test_vectors.py
@@ -2,275 +2,150 @@
 #
 import StlContainers
 
-print 'Now running tests from Python_test_vectors.py for type int..'
+print 'Now running unit tests from Python_test_vectors.py for type int..'
+
+errors = 0
+tests = 0
+
+def equals(a, b):
+	global errors
+	if(not (a == b)):
+		print "Error in "
+		print "Left operand value: " + str(a)
+		print "Right operand value: " + str(b)
+		errors += 1
+
+
+def container_creation_successful():
+	global tests
+	vector_int = StlContainers.vector_int()
+	equals(vector_int.__len__(), 0)
+	tests += 1
+
+def container_append_successful():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	equals(vector_int.__len__(), 1)
+	tests += 1
+
+def container_deletion_successful():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	vector_int.__delitem__(0)
+	equals(vector_int.__len__(), 0)
+	tests += 1
+
+def container_index_operator_works():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	equals(vector_int[0], 1)
+	tests += 1
+
+def container_deletion2_successful():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	vector_int.append(2)
+	vector_int.append(3)
+	vector_int.__delitem__(1)
+	equals(vector_int[0], 1)
+	equals(vector_int[1], 3)
+	tests += 1
+
+def container_clear_successful():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	vector_int.append(2)
+	vector_int.append(3)
+	vector_int.clear()
+	equals(vector_int.__len__(), 0)
+	tests += 1
+
+def container_contains_correct():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	equals(vector_int.__contains__(1), 1)
+	tests += 1
+
+def container_indexing_correct():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	equals(vector_int.index(1), 0)
+	tests += 1
+
+def container_count_correct():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	equals(vector_int.count(1), 1)
+	tests += 1
+
+def container_reverse_correct():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(1)
+	vector_int.append(2)
+	vector_int.append(3)
+	vector_int.reverse()
+	equals(vector_int[0], 3)
+	equals(vector_int[1], 2)
+	equals(vector_int[2], 1)
+	tests += 1
+
+def container_sort_correct():
+	global tests
+	vector_int = StlContainers.vector_int()
+	vector_int.append(3)
+	vector_int.append(1)
+	vector_int.append(2)
+	vector_int.sort()
+	equals(vector_int[0], 1)
+	equals(vector_int[1], 2)
+	equals(vector_int[2], 3)
+	tests += 1
+
+def container_stress_append_and_delete_correct():
+	global tests
+	vector_int = StlContainers.vector_int()
+	i = 0
+	while i < 10000:
+		vector_int.append(i)
+		i += 1
+	
+	equals(vector_int.__len__(), 10000)
+
+	i = 5000
+	while i > 0:
+		vector_int.__delitem__(i)
+		i -= 1
+	
+	equals(vector_int.__len__(), 5000)
+
+	vector_int.clear()
+	
+	equals(vector_int.__len__(), 0)
+	tests += 1
+
+
+container_creation_successful()
+container_append_successful()
+container_deletion_successful()
+container_index_operator_works()
+container_deletion2_successful()
+container_clear_successful()
+container_contains_correct()
+container_indexing_correct()
+container_count_correct()
+container_reverse_correct()
+container_sort_correct()
+container_stress_append_and_delete_correct()
+
+print str(tests) + " tests run."
+print str(errors) + " errors found."
 
-vector_int = StlContainers.vector_int()
-passed = 1
-
-if vector_int.__len__() != 0:
-	print "Error: newly created container isn't empty\n"
-	passed = 0
-
-vector_int.append(1);
-
-if vector_int.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_int.__len__() != 1:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-vector_int.__delitem__(0)
-
-if vector_int.__len__() != 0:
-	print "Error: container contains element(s) while it shouldn't\n"
-	passed = 0
-
-vector_int.append(1);
-
-if vector_int.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_int.__len__() != 1:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-vector_int.append(2);
-vector_int.append(3);
-
-if vector_int.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_int.__len__() != 3:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-if vector_int[0] != 1:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_int[1] != 2:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_int[2] != 3:
-	print "Error: different element was expected\n"
-	passed = 0
-
-vector_int[1] = -1                                               
-
-if vector_int.__contains__(-1) == False:
-	print "Error: element is not present"
-	passed = 0
-
-if vector_int[1] != -1:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_int.index(3) != 2:
-	print "Error: indexing problem\n"
-	passed = 0
-
-if vector_int.count(3) != 1:
-	print "Error: counting problem\n"
-	passed = 0
-
-vector_int.reverse()
-
-if vector_int[0] != 3:
-	print "Error: reversing problem\n"
-	passed = 0
-
-vector_int.sort()
-
-if vector_int[0] != -1:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_int[1] != 1:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_int[2] != 3:
-	print "Error: different element was expected\n"
-	passed = 0
-
-vector_int.clear()
-
-if vector_int.__len__() != 0:
-	print "Error: after clear() container isn't empty\n"
-	passed = 0
-
-if passed:
-	print "All tests for vector (int) passed!"
-else:
-	print "Error found!"
-
-#
-# Doubles
-#
-
-print 'Now running tests from Python_test_vectors.py for type double..'
-
-vector_double = StlContainers.vector_double()
-passed = 1
-
-if vector_double.__len__() != 0:
-	print "Error: newly created container isn't empty\n"
-	passed = 0
-
-vector_double.append(1.5);
-
-if vector_double.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_double.__len__() != 1:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-vector_double.__delitem__(0)
-
-if vector_double.__len__() != 0:
-	print "Error: container contains element(s) while it shouldn't\n"
-	passed = 0
-
-vector_double.append(1.9);
-
-if vector_double.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_double.__len__() != 1:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-vector_double.append(2.2);
-vector_double.append(3.3);
-
-if vector_double.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_double.__len__() != 3:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-if vector_double[0] != 1.9:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_double[1] != 2.2:
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_double[2] != 3.3:
-	print "Error: different element was expected\n"
-	passed = 0
-
-vector_double[2] = -1.7                                               
-
-if vector_double.__contains__(-1.7) == False:
-	print "Error: element is not present"
-	passed = 0
-
-if vector_double[2] != -1.7:
-	print "Error: different element was expected\n"
-	passed = 0
-
-vector_double.clear()
-
-if vector_double.__len__() != 0:
-	print "Error: after clear() container isn't empty\n"
-	passed = 0
-
-if passed:
-	print "All tests for vector (double) passed!"
-else:
-	print "Error found!"
-
-#
-# Strings
-#
-
-print 'Now running tests from Python_test_vectors.py for type string..'
-
-vector_string = StlContainers.vector_string()
-passed = 1
-
-if vector_string.__len__() != 0:
-	print "Error: newly created container isn't empty\n"
-	passed = 0
-
-vector_string.append("AAA");
-
-if vector_string.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_string.__len__() != 1:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-vector_string.__delitem__(0)
-
-if vector_string.__len__() != 0:
-	print "Error: container contains element(s) while it shouldn't\n"
-	passed = 0
-
-vector_string.append("AAB");
-
-if vector_string.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_string.__len__() != 1:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-vector_string.append("BBB");
-vector_string.append("CCC");
-
-if vector_string.__len__() == 0:
-	print "Error: container is empty while it shouldn't be\n"
-	passed = 0
-
-if vector_string.__len__() != 3:
-	print "Error: container size is incorrect\n"
-	passed = 0
-
-if vector_string[0] != "AAB":
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_string[1] != "BBB":
-	print "Error: different element was expected\n"
-	passed = 0
-
-if vector_string[2] != "CCC":
-	print "Error: different element was expected\n"
-	passed = 0
-
-vector_string[2] = "UPS"                                               
-
-if vector_string.__contains__("UPS") == False:
-	print "Error: element is not present"
-	passed = 0
-
-if vector_string[2] != "UPS":
-	print "Error: different element was expected\n"
-	passed = 0
-
-vector_string.clear()
-
-if vector_string.__len__() != 0:
-	print "Error: after clear() container isn't empty\n"
-	passed = 0
-
-if passed:
-	print "All tests for vector (string) passed!"
-else:
-	print "Error found!"
