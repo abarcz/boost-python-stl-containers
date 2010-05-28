4050e111ddfb83ba6b33cb8d4335163518c384e2
abarcz <abarcz@77f40421-bdcf-6194-5f83-fd0082402d5a>
2010-05-28 14:16:18 +0000

    Zmienione nazwy dla testów konwerterów. Teraz testują one zarówno przejście Python->C++ (kontrola typów) jak i w drugą stronę.

---
+++ trunk/Source/Python_test_py_list_vector_conversions.py
@@ -0,0 +1,71 @@
+# Author: Aleksy Barcz
+#
+from StlContainers import *
+import TestModule
+
+print 'Now running tests from Python_test_py_list_vector_conversions.py..'
+
+errors = 0
+tests = 0
+
+def equals(a, b):
+	global errors
+	if(not (a == b)):
+		print "Error in "
+		print "Left operand value: " + str(a)
+		print "Right operand value: " + str(b)
+		errors += 1
+        
+def elem_sum(list):
+    sum = 0
+    for e in list:
+        sum += e
+    return sum    
+        
+def list_conversion_sum_int():
+    global tests
+    result_list = [1, 2321, 124523]
+    source_list = [1, 'c', 2321, 'text', 0.56, 124523]
+    test_sum = TestModule.get_vector_sum_int(py_list_to_vector_int(source_list))
+    correct_sum = elem_sum(result_list)
+    equals(test_sum, correct_sum)
+    tests += 1
+    
+def list_conversion_sum_double():
+    global tests
+    result_list = [1, 1.54, 2321, 0.56, 124523, 124.587]
+    source_list = [1, 'c', 1.54, 2321, 'text', 0.56, 124523, 124.587]
+    test_sum = TestModule.get_vector_sum_double(py_list_to_vector_double(source_list))
+    correct_sum = elem_sum(result_list)
+    equals(test_sum, correct_sum)
+    tests += 1
+    
+def list_conversion_int():
+    global tests
+    result_list = [1, 2321, 124523]
+    source_list = [1, 'c', 2321, 'text', 0.56, 124523]
+    equals(py_list_to_vector_int(source_list).get_list(), result_list)
+    tests += 1
+    
+def list_conversion_double():
+    global tests
+    result_list = [1, 1.54, 2321, 0.56, 124523, 124.587]
+    source_list = [1, 'c', 1.54, 2321, 'text', 0.56, 124523, 124.587]
+    equals(py_list_to_vector_double(source_list).get_list(), result_list)
+    tests += 1
+    
+def list_conversion_string():
+    global tests
+    result_list = ['c', 'text']
+    source_list = [1, 'c', 1.54, 2321, 'text', 0.56, 124523, 124.587]
+    equals(py_list_to_vector_string(source_list).get_list(), result_list)
+    tests += 1
+
+list_conversion_sum_int()
+list_conversion_sum_double()
+list_conversion_int()
+list_conversion_double()
+list_conversion_string()
+
+print str(tests) + " tests run."
+print str(errors) + " errors found."
