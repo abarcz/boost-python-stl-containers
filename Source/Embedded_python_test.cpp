d6cd627058fd08caa56e3e3e586711c491773678
abarcz <abarcz@77f40421-bdcf-6194-5f83-fd0082402d5a>
2010-05-28 14:16:18 +0000

    Zmienione nazwy dla testów konwerterów. Teraz testują one zarówno przejście Python->C++ (kontrola typów) jak i w drugą stronę.

--- Source/Embedded_python_test.cpp
+++ Source/Embedded_python_test.cpp
@@ -69,11 +69,11 @@ int main() {
         exec_file("Python_test_vector_foobar.py", globals, globals);
         
         //converters test from external .py files
-        std::cout << "\nLaunching Python tests from Python_test_py_list_to_vector.py..\n";
-        exec_file("Python_test_py_list_to_vector.py", globals, globals);
+        std::cout << "\nLaunching Python tests from Python_test_py_list_vector_conversions.py..\n";
+        exec_file("Python_test_py_list_vector_conversions.py", globals, globals);
         
-        std::cout << "\nLaunching Python tests from Python_test_py_dict_to_map.py..\n";
-        exec_file("Python_test_py_dict_to_map.py", globals, globals);
+        std::cout << "\nLaunching Python tests from Python_test_py_dict_map_conversions.py..\n";
+        exec_file("Python_test_py_dict_map_conversions.py", globals, globals);
         
         std::cout << "Embedded Python tests terminated.\n";
     } 
---
+++ Source/Python_test_py_dict_map_conversions.py
@@ -0,0 +1,31 @@
+# Author: Aleksy Barcz
+#
+from StlContainers import *
+from TestModule import *
+
+print 'Now running tests from Python_test_py_dict_map_conversions.py..'
+
+tests = 0
+errors = 0
+
+def equals(a, b):
+	global errors
+	if(not (a == b)):
+		print "Error in "
+		print "Left operand value: " + str(a)
+		print "Right operand value: " + str(b)
+		errors += 1
+
+d = {'try' : 'tyu', 67 : 'ddd', 2.7 : 5.65, 33 : 0.897, 54 : 'dasda'}
+
+equals(py_dict_to_map_int_string(d).get_dict(), {54: 'dasda', 67: 'ddd'})
+tests += 1
+
+equals(py_dict_to_map_string_string(d).get_dict(), {'try': 'tyu'})
+tests += 1
+
+equals(py_dict_to_map_int_double(d).get_dict(), {33: 0.897})
+tests += 1
+
+print str(tests) + " tests run."
+print str(errors) + " errors found."
