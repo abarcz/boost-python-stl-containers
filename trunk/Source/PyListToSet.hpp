4b2bd4aa5e85833b0ba5b92c11e33314779076ac
abarcz@gmail.com <abarcz@gmail.com@77f40421-bdcf-6194-5f83-fd0082402d5a>
2010-06-07 11:40:44 +0000

    Poprawiony doxygen, @author w dwóch plikach

--- trunk/Source/PyListToSet.hpp
+++ trunk/Source/PyListToSet.hpp
@@ -1,6 +1,7 @@
 /**
- * Author: Aleksy Barcz
  * Converts a boost::python::list to std::set
+ *
+ * @author: Aleksy Barcz
  */
 #ifndef STL_TO_PYTHON_CONTAINERS_LIST_SET_CONVERTER
 #define STL_TO_PYTHON_CONTAINERS_LIST_SET_CONVERTER
